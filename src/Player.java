5e4c28f072cbc5315149e1d2d1215334f93721fc
Frequill <89257706+Frequill@users.noreply.github.com>
2021-10-06 10:53:01 +0200

    Julius är inte bara skitsnygg utan även klar med koden! ;)

--- src/Player.java
+++ src/Player.java
@@ -5,51 +5,69 @@ import java.util.Scanner;
 
 public class Player {
     public static void main(String[] args) throws Exception {
-       // readUsername();
-        writeUsername();
-
-
-
+        readUsername();
     }
+
+
+
+    /**
+    This method stores different usernames in a textile for later use
+     */
+
     static void writeUsername() throws Exception {
         Scanner in = new Scanner(System.in);
-        System.out.println("Please enter username ");
+        System.out.println("Please enter your username: ");
         String usersInput = in.nextLine();
+
         File toPlaceholder = new File("src/thePlaceholder.txt");
+        File toUsername = new File("src/username.txt");
+
+        //This Printwriter stores the users username in the "thePlaceholder" textfile
         PrintWriter output = new PrintWriter(toPlaceholder);
         output.println(usersInput);
         output.close();
-        File usernames = new File("src/username.txt");
 
-        Scanner scanner = new Scanner(new File("src/thePlaceholder.txt"));
-        Scanner scanner2 = new Scanner(new File("src/username.txt"));
-        String words = scanner.nextLine();
-        String toUsername = scanner2.nextLine();
+        Scanner readPlaceholder = new Scanner(toPlaceholder);
+        Scanner readUsername = new Scanner(toUsername);
+        String newUsername = readPlaceholder.nextLine();
+        String usernameSaver = readUsername.nextLine();
+        readPlaceholder.close();
 
-       // while (scanner.hasNextLine()){
-
-         //   System.out.println(words);
-      //  }
-        scanner.close();
-
-        for(int i = 0; i < words.length(); i++){
+        for(int i = 0; i < newUsername.length(); i++){
         PrintWriter out = new PrintWriter ("src/username.txt");
-        out.println(  words  +  " " + toUsername );
+        out.println( newUsername  +  " " + usernameSaver );
         out.close();
         }
-
-
     }
 
-    static void readUsername() throws Exception {
-        System.out.println("Welcome!\nPlease add new user\nOr select existing user: ");
-        File usernames = new File("src/username.txt");
-        Scanner readUsernames = new Scanner(usernames);
 
-        while (readUsernames.hasNextLine()){
-            String linePrinter = readUsernames.nextLine();
-            System.out.println("\n" + linePrinter);
+
+    /**
+     This method shows the user all available usernames and allows user to select an already existing username
+     */
+
+    static void readUsername() throws Exception {
+        System.out.println("Please choose: \n1) Existing users\n2) Create new user");
+        Scanner input = new Scanner(System.in);
+        int userChoice = input.nextInt();
+
+        if (userChoice == 1){
+            File usernames = new File("src/username.txt");
+            Scanner readUsernames = new Scanner(usernames);
+
+
+            while (readUsernames.hasNextLine()){
+                String linePrinter = readUsernames.nextLine();
+                System.out.println("\n" + linePrinter);
+            }
         }
+        else if (userChoice == 2){
+            writeUsername();
+        }
+        else {
+            System.out.println("VAFAN GÖRRU???");
+        }
+
 
     }
 }
