df0eef36adbcf8c4f7b0dc00382d0ae91ee1ebab
Charlemain <matsonswe@gmail.com>
2021-10-07 13:37:16 +0200

    Martin är flummig

--- src/Game.java
+++ src/Game.java
@@ -11,36 +11,61 @@ public class Game {
 
             String placeholder = "Newton".toLowerCase();
             System.out.println("Welcome to Hang Man guess the word that is " + placeholder.length() + " letters long!");
+           boolean guessCorrect = false;
+           boolean guessIncorrect = false;
+            for(int i=0; i < placeholder.length();i++){
+                System.out.print("_ ");
+            }
+
             String letter = in.next().toLowerCase();
 
             for(int i = 0; i<placeholder.length(); i++){
                 if(letter.charAt(0) == placeholder.charAt(i)){
-                    System.out.println(letter);
+                   guessCorrect = true;
                 }
                 else if(letter.charAt(0) != placeholder.charAt(i)){
-                    System.out.println("_");
-                    lifeTaker(playerLife);
-                    victory = true;
+                    guessIncorrect = true;
                 }
                 else{
                     System.out.println("BLÖ");
 
                 }
             }
-            //victory = true; // placeholder
+            if(guessCorrect) {
+
+                String result = correctLetter(letter, placeholder);
+                System.out.println(result);
+            }
+            else if(guessIncorrect){
+                lifeTaker(playerLife);
+            }
+
+            victory = true; // placeholder
 
         }
     }
-
-
-
-
-
 
     public static int lifeTaker(int playerLife){
         playerLife = playerLife -1;
         System.out.println("Incorrect guess! You have lost one life!" + "\n(" + playerLife + " lives remaining)");
         return playerLife;
+    }
+    public static String correctLetter (String letter, String placeholder){
+
+        for(int i=0; i < placeholder.length();i++){
+            if(letter.charAt(i) == placeholder.charAt(i)){
+
+                return letter;
+            }
+            else if(letter.charAt(i) != placeholder.charAt(i)){
+                return letter;
+            }
+
+        }
+
+
+        return null;
+
     }
 
 }
