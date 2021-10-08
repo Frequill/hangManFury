2fa4b377754c042b34142138e7de3933ddab4461
Frequill <89257706+Frequill@users.noreply.github.com>
2021-10-08 10:52:57 +0200

    Julius är inte bara skitsnygg utan även klar med koden! ;)

--- src/Game.java
+++ src/Game.java
@@ -13,63 +13,58 @@ public class Game {
         System.out.println("Welcome to Hang Man guess the word that is " + placeholder.length() + " letters long!");
 
 
-        for(int i=0; i < placeholder.length();i++) {
+        for (int i = 0; i < placeholder.length(); i++) {
             System.out.print("_");
             allLetters.add(i, '_');
         }
 
-        while(victory == false&&playerLife > 0){
+        while (victory == false && playerLife > 0) {
             boolean guessCorrect = false;
             boolean guessIncorrect = false;
 
             String letter = in.next().toLowerCase();
 
-            for(int i = 0; i<placeholder.length(); i++){
-                if(letter.charAt(0) == placeholder.charAt(i)){
-                   guessCorrect = true;
-                }
-                else if(letter.charAt(0) != placeholder.charAt(i)){
+            for (int i = 0; i < placeholder.length(); i++) {
+                if (letter.charAt(0) == placeholder.charAt(i)) {
+                    guessCorrect = true;
+                } else if (letter.charAt(0) != placeholder.charAt(i)) {
                     guessIncorrect = true;
-                }
-                else{
+                } else {
                     System.out.println("BLÖ");
                 }
             }
 
-            if(guessCorrect) {
+            if (guessCorrect) {
                 correctLetter(letter, placeholder, allLetters);
-            }
-            else if(guessIncorrect){
-                //lifeTaker(playerLife);
+            } else if (guessIncorrect) {
                 playerLife = playerLife - 1;
                 System.out.println("Incorrect guess! You have lost one life!" + "\n(" + playerLife + " lives remaining)");
             }
-          //  victory = true; // placeholder
+            if (playerLife == 0){
+                System.out.println("\nYou have been defeated! (Scrub)");
+            }
         }
     }
 
-    public static void correctLetter (String letter, String placeholder, ArrayList<Character> allLetters){
+    public static void correctLetter(String letter, String placeholder, ArrayList<Character> allLetters) {
         char guess = letter.charAt(0);
 
-
-        for(int i=0; i < placeholder.length(); i++) {
+        for (int i = 0; i < placeholder.length(); i++) {
             if (guess == placeholder.charAt(i)) {
                 allLetters.set(i, guess);
                 //System.out.print(allLetters.get(i));
             }
         }
-            for(int j = 0; j < allLetters.size(); j++) {
-                System.out.print(allLetters.get(j));
-            }
-            char underscore = '_';
-        for(int i = 0; i < placeholder.length(); i++)
-            if (allLetters.get(i) == underscore){
-
-            }else{
-                System.out.println("Victory ");
-                System.exit(0);
-            }
+        for (int j = 0; j < allLetters.size(); j++) {
+            System.out.print(allLetters.get(j));
+        }
+        if(allLetters.contains('_')){
 
         }
+        else{
+            System.out.println("\n\nVictory! :)");
+            System.exit(0);
+        }
     }
+}
 
