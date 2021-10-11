c085c248d3a4b92b774e370d2011930af706ef33
Frequill <89257706+Frequill@users.noreply.github.com>
2021-10-11 13:58:26 +0200

    BLÖ

--- src/Menu.java
+++ src/Menu.java
@@ -2,40 +2,57 @@ import java.util.Scanner;
 
 class Menu{
 
-    public static void main(String[] args) throws Exception  {
+    public static void main(String[] args) throws Exception {
         System.out.println("Welcome to Hang man!!");
         show();
         Scanner choiceInput = new Scanner(System.in);
 
         String user = null;
         boolean run = true;
-        while(run) {
-            int choice = choiceInput.nextInt();
-            switch (choice) {
-                case 1:
-                    user = Player.readUsername(user);
-                    showPlusUser(user);
-                    break;
-                case 2:
-                    if (user== null) {
-                        System.out.println("Please select a user first!!!!!!!\n(Press Enter to return to menu)");
-                        choiceInput.nextLine();
-                        choiceInput.nextLine();
-                        show();
 
-                    } else {
-                        Game.hangMan(user);
-                    }
-                    break;
-                case 3:
-                    System.out.println("Thank you for playing!!");
-                    run = false;
-                    break;
-                default:
-                    System.out.println("NOOOOO!!");
+        while (run) {
+            int choice = 1;
+
+            try {
+                choice = choiceInput.nextInt();
+            } catch (Exception e) {
+                System.out.println("Invalid input! Please enter a number between 1-3\n");
+                choiceInput.nextLine();
+                show();
+            }
+
+            if (choice == 1) {
+                user = Player.readUsername(user);
+                showPlusUser(user);
+                break;
+            }
+                else if (choice == 2) {
+                if (user == null) {
+                    System.out.println("Please select a user first!!!!!!!\n(Press Enter to return to menu)");
+                    choiceInput.nextLine();
+                    choiceInput.nextLine();
+                    show();
+                } else {
+                    Game.hangMan(user);
+                }
+                break;
+            }
+                else if (choice == 3) {
+                System.out.println("Shutting down...");
+                run = false;
+                break;
+            }
+                else if (choice < 1){
+                System.out.println("Please enter a number greater than 0...");
+                show();
+               }
+                else if (choice > 3){
+                System.out.println("Please enter a number lower than 3...");
+                show();
             }
         }
     }
+
     static void show(){
         System.out.println("Please choose:");
         System.out.println("1) Select user");
